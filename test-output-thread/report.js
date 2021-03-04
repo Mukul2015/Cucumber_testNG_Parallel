cb9ee0701c15f3000ca4f7708f0c979752c972aa
mukul <mukul_wana@yahoo.com>
2021-03-03 20:28:05 -0500

    Parallel test

---
+++ test-output-thread/report.js
@@ -0,0 +1,111 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "f3b194f5-557a-465d-9f6d-017469ff23e3",
+    "feature": "Contact Us Feature",
+    "scenario": "Contact Us scenario with different set of data",
+    "start": 1614745078363,
+    "group": 19,
+    "content": "",
+    "tags": "",
+    "end": 1614745128115,
+    "className": "passed"
+  },
+  {
+    "id": "e06d1901-16fc-4631-a7a9-09819a543edf",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts section count",
+    "start": 1614745078365,
+    "group": 18,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1614745128132,
+    "className": "passed"
+  },
+  {
+    "id": "14800841-e00a-4dd9-9e9b-868c49595237",
+    "feature": "Login page feature",
+    "scenario": "Login with correct credentials",
+    "start": 1614745078394,
+    "group": 23,
+    "content": "",
+    "tags": "",
+    "end": 1614745122598,
+    "className": "passed"
+  },
+  {
+    "id": "8414277b-6414-4356-b29c-e45e2f9ba097",
+    "feature": "Contact Us Feature",
+    "scenario": "Contact Us scenario with different set of data",
+    "start": 1614745078401,
+    "group": 20,
+    "content": "",
+    "tags": "",
+    "end": 1614745127509,
+    "className": "passed"
+  },
+  {
+    "id": "360e226d-ced6-4ca8-b023-47d096fccb9a",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts page title",
+    "start": 1614745078397,
+    "group": 17,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1614745128194,
+    "className": "passed"
+  },
+  {
+    "id": "c1514b60-1152-45ce-b0d3-23c9b656c4d6",
+    "feature": "Login page feature",
+    "scenario": "Forgot Password link",
+    "start": 1614745078236,
+    "group": 22,
+    "content": "",
+    "tags": "",
+    "end": 1614745122619,
+    "className": "passed"
+  },
+  {
+    "id": "cba713e6-8fa9-4b58-aed5-15113171cae9",
+    "feature": "Login page feature",
+    "scenario": "Login page title",
+    "start": 1614745078402,
+    "group": 21,
+    "content": "",
+    "tags": "",
+    "end": 1614745123587,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 17,
+    "content": "Thread[TestNG-PoolService-0,5,main]"
+  },
+  {
+    "id": 18,
+    "content": "Thread[TestNG-PoolService-1,5,main]"
+  },
+  {
+    "id": 19,
+    "content": "Thread[TestNG-PoolService-2,5,main]"
+  },
+  {
+    "id": 20,
+    "content": "Thread[TestNG-PoolService-3,5,main]"
+  },
+  {
+    "id": 21,
+    "content": "Thread[TestNG-PoolService-4,5,main]"
+  },
+  {
+    "id": 22,
+    "content": "Thread[TestNG-PoolService-5,5,main]"
+  },
+  {
+    "id": 23,
+    "content": "Thread[TestNG-PoolService-6,5,main]"
+  }
+]);
+});
